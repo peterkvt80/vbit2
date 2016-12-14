987434c5ff55918a3fc56a5fbb9dce4c64becb41
Peter Kwan <peterk.vt80@gmail.com>
2016-12-14 22:23:59 +0000

    Set header time to yellow

--- mag.cpp
+++ mag.cpp
@@ -187,7 +187,7 @@ Packet* Mag::GetPacket(Packet* p)
     // p=new Packet();
     p->Header(_magNumber,thisPageNum,thisSubcode,thisStatus);// loads of stuff to do here!
 
-    p->HeaderText("CEEFAX 1 %%# %%a %d %%b 12:34.56"); // Placeholder 32 characters. This gets replaced later
+    p->HeaderText("CEEFAX 1 %%# %%a %d %%b" "\x03" "12:34.56"); // Placeholder 32 characters. This gets replaced later
 
 
     p->Parity(13);
