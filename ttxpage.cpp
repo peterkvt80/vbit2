95f2aab9adbd0aa0c7718cd7781c8657cc202fc6
ZXGuesser <alistair@zxnet.co.uk>
2018-05-23 22:21:17 +0100

    inherit status from previous sub page if PS row is missing instead of reverting to default.

--- ttxpage.cpp
+++ ttxpage.cpp
@@ -512,9 +512,11 @@ bool TTXPage::m_LoadTTI(std::string filename)
                     if (p->m_PageNumber!=FIRSTPAGE) // // Subsequent pages need new page instances
                     {
                         // std::cerr << "Created a new subpage" << std::endl;
+                        int pagestatus = p->GetPageStatus();
                         TTXPage* newSubPage=new TTXPage();  // Create a new instance for the subpage
                         p->Setm_SubPage(newSubPage);            // Put in a link to it
                         p=newSubPage;                       // And jump to the next subpage ready to populate
+                        p->SetPageStatus(pagestatus); // inherit status of previous page instead of default
                     }
                     p->SetPageNumber(pageNumber);
 
