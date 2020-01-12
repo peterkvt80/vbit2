f11e6e62968b3748e91b3f61ab3b39af4123e78c
ZXGuesser <alistair@zxnet.co.uk>
2020-01-12 13:22:15 +0000

    add update to vbit-config

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo Updating to latest stable version of vbit2
+echo Updating to latest stable version of VBIT2.
 git fetch --tags
 latestTag=`curl --silent "https://api.github.com/repos/peterkvt80/vbit2/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
-git checkout $latestTag # switch to latest 
+git checkout $latestTag --quiet # switch to latest 
 
 # new version might need to perform tasks we don't know about, so run its postupdate script
 ./postupdate.sh
